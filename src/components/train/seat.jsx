1bed3cc4c30d0fca3bd4aba65975da4d600e67ff
Akash Maurya <mauryaopean8758@gmail.com>
2023-06-24 14:11:37 +0530

    yodate

--- src/components/train/seat.jsx
+++ src/components/train/seat.jsx
@@ -6,7 +6,7 @@ function SeatComponent({
   isBooked,
   isRecentlyBooked,
 }) {
-  return (
+  return ( 
     <div className={` relative w-20 h-20 border-2 m-1 p-2 hover:bg-slate-200 ${(isBooked && !isRecentlyBooked) && "bg-slate-200 " } ${isRecentlyBooked && "bg-green-300" } `}>
       <div className="absolute bottom-2 right-2 text-xs">
         R{rowId}-{colId} ({seatId})
